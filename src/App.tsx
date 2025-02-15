4b38a1f69e42a6a0630adc44f3a7b2811c072699
sahil gupta <gsahil438@gmail.com>
2025-02-15 11:51:52 +0530

    product mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import Favorites from './components/Favorites';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div>
         <Routes>
           <Route path='/' element={<ProductList />} />
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from 'react';
+
 const ProductList = () => {
-  return <div>product list</div>;
+  const [products, setProducts] = useState<any>(null);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+  return (
+    <div>
+      {products &&
+        products.map((product: any) => (
+          <div key={product.id}>
+            <img height='100px' width='100px' src={product.image} alt='' />
+            <div>{product.title}</div>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default ProductList;
